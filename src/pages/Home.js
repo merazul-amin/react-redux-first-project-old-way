80ab91f9bd824082bc81150c1f961abb20abd13b
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-01-28 08:36:04 +0600

    add cart page initialized

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
       {
-        products.map(product => <ProductCard product={product}></ProductCard>)
+        products.map(product => <ProductCard product={product} key={product._id}></ProductCard>)
       }
     </div>
   );
